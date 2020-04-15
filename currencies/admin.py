8194b2bc0e779f4041241a16159274a34eb78042
Daria Zakharova <zakharovadaria96@gmail.com>
2020-04-15 18:15:01 +0300

    Create model Currency

--- currencies/admin.py
+++ currencies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from currencies.models import Currency
+
+admin.site.register(Currency)
--- currencies/models.py
+++ currencies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=64, unique=True, db_index=True)
+    rate = models.DecimalField(decimal_places=4, max_digits=7)
+
+    def __str__(self):
+        return f"{self.name} - {self.rate}"
