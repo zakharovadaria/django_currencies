39be683583af83e92eeba67c5025381c5db26386
Daria Zakharova <zakharovadaria96@gmail.com>
2020-04-16 11:41:21 +0300

    Add simple api for import currencies

--- currencies/urls.py
+++ currencies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from currencies.views import CurrenciesListView, CurrenciesDetailView
+from currencies.views import CurrenciesListView, CurrenciesDetailView, CurrenciesImportView
 
 urlpatterns = [
     path("currencies/", CurrenciesListView.as_view()),
     path("currencies/<int:pk>", CurrenciesDetailView.as_view()),
+    path("currencies/import", CurrenciesImportView.as_view()),
 ]
--- currencies/views.py
+++ currencies/views.py
@@ -1,4 +1,7 @@
+import requests
+from xml.etree import ElementTree
 from rest_framework import generics
+from rest_framework.response import Response
 
 from currencies.models import Currency
 from currencies.serializers import CurrencySerializer
@@ -13,3 +16,25 @@ class CurrenciesDetailView(generics.RetrieveAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
 
+
+class CurrenciesImportView(generics.GenericAPIView):
+    def get(self, request):
+        r = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")
+        root = ElementTree.fromstring(r.text)
+        for child in root:
+            name = None
+            value = None
+
+            for ch in child:
+                if ch.tag.lower() == "name":
+                    name = ch.text
+                if ch.tag.lower() == "value":
+                    value = ch.text.replace(",", ".")
+                    value = float(value)
+
+            if name and value:
+                if not Currency.objects.filter(name=name):
+                    currency = Currency(name=name, rate=value)
+                    currency.save()
+
+        return Response({"result": "true"})
